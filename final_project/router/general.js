769554ac80c03a71c01428960871007bbd325b2b
mariiam-aly <xmariam.aly@gmail.com>
2024-12-14 08:54:15 -0500

    fix register

--- final_project/router/general.js
+++ final_project/router/general.js
@@ -20,6 +20,7 @@ const doesExist = (username) => {
 public_users.post("/register", (req,res) => {
     const username = req.body.username;
     const password = req.body.password;
+console.log(req.body)
     // Check if both username and password are provided
     if (username && password) {
         // Check if the user does not already exist
